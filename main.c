c62c3b9966c903de5f0e622d10714da1b8a47f72
Sudharshan-3904 <sudharshanvalar@gmail.com>
2024-12-14 18:23:30 +0530

    draft commit

--- main.c
+++ main.c
@@ -1,220 +1,226 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_BOOKED_FLIGHTS 10
+#include <string.h>
+
+#define MAX_FLIGHTS 100
+#define MAX_STR_LEN 100
 
 typedef struct {
-    char flight_no[10];
-    char airlines[50];
-    char origin[50];
-    char destination[50];
-    char departure_time[20];
-    char arrival_time[20];
-    int flight_hours;
+    char flightNo[MAX_STR_LEN];
+    char airline[MAX_STR_LEN];
+    char origin[MAX_STR_LEN];
+    char destination[MAX_STR_LEN];
+    char departureTime[MAX_STR_LEN];
+    char arrivalTime[MAX_STR_LEN];
+    float flightHours;
     float price;
-    int seats_left;
-    int seats_booked;
+    int seatsLeft;
+    int seatsBooked;
 } Flight;
 
-Flight booked_flights[MAX_BOOKED_FLIGHTS];
-int num_booked_flights = 0;
-
-// Function to read flights from a file
-void load_flights(const char *filename) {
+void loadFlights(const char *filename, Flight flights[], int *count) {
     FILE *file = fopen(filename, "r");
     if (!file) {
-        printf("Cannot open file %s\n", filename);
+        printf("Error: Could not open file %s\n", filename);
         return;
     }
 
-    while (fscanf(file, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%d,%f,%d,%d",
-                   booked_flights[num_booked_flights].flight_no,
-                   booked_flights[num_booked_flights].airlines,
-                   booked_flights[num_booked_flights].origin,
-                   booked_flights[num_booked_flights].destination,
-                   booked_flights[num_booked_flights].departure_time,
-                   booked_flights[num_booked_flights].arrival_time,
-                   &booked_flights[num_booked_flights].flight_hours,
-                   &booked_flights[num_booked_flights].price,
-                   &booked_flights[num_booked_flights].seats_left,
-                   &booked_flights[num_booked_flights].seats_booked) == 10) {
-        num_booked_flights++;
+    *count = 0;
+    while (fscanf(file, "%[^,], %[^,], %[^,], %[^,], %[^,], %[^,], %f, %f, %d, %d\n",
+                  flights[*count].flightNo, flights[*count].airline, flights[*count].origin,
+                  flights[*count].destination, flights[*count].departureTime, flights[*count].arrivalTime,
+                  &flights[*count].flightHours, &flights[*count].price,
+                  &flights[*count].seatsLeft, &flights[*count].seatsBooked) != EOF) {
+        (*count)++;
     }
 
     fclose(file);
 }
 
-// Function to write booked flights to a file
-void save_flights(const char *filename) {
+void saveFlights(const char *filename, Flight flights[], int count) {
     FILE *file = fopen(filename, "w");
     if (!file) {
-        printf("Cannot open file %s\n", filename);
+        printf("Error: Could not open file %s\n", filename);
         return;
     }
 
-    for (int i = 0; i < num_booked_flights; ++i) {
-        fprintf(file, "%s,%s,%s,%s,%s,%s,%d,%.2f,%d,%d\n",
-                booked_flights[i].flight_no,
-                booked_flights[i].airlines,
-                booked_flights[i].origin,
-                booked_flights[i].destination,
-                booked_flights[i].departure_time,
-                booked_flights[i].arrival_time,
-                booked_flights[num_booked_flights].flight_hours,
-                booked_flights[num_booked_flights].price,
-                booked_flights[num_booked_flights].seats_left,
-                booked_flights[num_booked_flights].seats_booked);
+    for (int i = 0; i < count; i++) {
+        fprintf(file, "%s, %s, %s, %s, %s, %s, %.2f, %.2f, %d, %d\n",
+                flights[i].flightNo, flights[i].airline, flights[i].origin, flights[i].destination,
+                flights[i].departureTime, flights[i].arrivalTime, flights[i].flightHours,
+                flights[i].price, flights[i].seatsLeft, flights[i].seatsBooked);
     }
 
     fclose(file);
 }
 
-// Function to display available flights
-void display_flights() {
-    if (num_booked_flights == 0) {
-        printf("No flights are currently available.\n");
+void viewBookedFlights() {
+    Flight flights[MAX_FLIGHTS];
+    int count;
+    loadFlights("bookedFlights.txt", flights, &count);
+
+    if (count == 0) {
+        printf("No booked flights available.\n");
         return;
     }
 
-    printf("\nAvailable Flights:\n");
-    for (int i = 0; i < num_booked_flights; ++i) {
-        printf("%d. Flight No: %s, Airlines: %s, Origin: %s, Destination: %s, Departure Time: %s, Arrival Time: %s, Flight Hours: %d, Price: %.2f\n",
-               i + 1,
-               booked_flights[i].flight_no,
-               booked_flights[i].airlines,
-               booked_flights[i].origin,
-               booked_flights[i].destination,
-               booked_flights[i].departure_time,
-               booked_flights[i].arrival_time,
-               booked_flights[i].flight_hours,
-               booked_flights[i].price);
+    printf("Booked Flights:\n");
+    for (int i = 0; i < count; i++) {
+        printf("%d. %s (%s) - %s to %s\n",
+               i + 1, flights[i].flightNo, flights[i].airline,
+               flights[i].origin, flights[i].destination);
     }
 }
 
-// Function to add a new flight
-void book_flight() {
-    if (num_booked_flights >= MAX_BOOKED_FLIGHTS) {
-        printf("Cannot book more flights, database is full.\n");
+void bookNewFlight() {
+    Flight allFlights[MAX_FLIGHTS], bookedFlights[MAX_FLIGHTS];
+    int allCount, bookedCount;
+    loadFlights("allFlights.txt", allFlights, &allCount);
+    loadFlights("bookedFlights.txt", bookedFlights, &bookedCount);
+
+    if (allCount == 0) {
+        printf("No available flights to book.\n");
         return;
     }
 
-    display_flights();
+    printf("Available Flights:\n");
+    for (int i = 0; i < allCount; i++) {
+        printf("%d. %s (%s) - %s to %s, $%.2f, Seats Left: %d\n",
+               i + 1, allFlights[i].flightNo, allFlights[i].airline,
+               allFlights[i].origin, allFlights[i].destination,
+               allFlights[i].price, allFlights[i].seatsLeft);
+    }
 
     int choice;
     printf("Enter the number of the flight you want to book: ");
     scanf("%d", &choice);
 
-    if (choice < 1 || choice > num_booked_flights) {
-        printf("Invalid choice. Please try again.\n");
+    if (choice < 1 || choice > allCount) {
+        printf("Invalid choice.\n");
         return;
     }
 
-    // Decrease seats left and increase seats booked
-    booked_flights[choice - 1].seats_left--;
-    booked_flights[choice - 1].seats_booked++;
-
-    printf("\nFlight booked successfully!\n");
-}
-
-// Function to delete a booked flight
-void delete_flight() {
-    char flight_no[10];
-    printf("Enter Flight No of the flight you want to delete: ");
-    scanf("%s", flight_no);
-    for (int i = 0; i < num_booked_flights; ++i) {
-        if (strcmp(booked_flights[i].flight_no, flight_no) == 0) {
-            for (int j = i; j < num_booked_flights - 1; ++j) {
-                booked_flights[j] = booked_flights[j + 1];
-            }
-            --num_booked_flights;
-            printf("Flight deleted successfully.\n");
-            save_flights("flights.txt");
-            return;
-        }
-    }
-    printf("No flight found with this Flight No.\n");
-}
-
-// Function to view all booked flights
-void view_flights() {
-    if (num_booked_flights == 0) {
-        printf("No flights are currently booked.\n");
+    Flight *selectedFlight = &allFlights[choice - 1];
+    if (selectedFlight->seatsLeft == 0) {
+        printf("No seats left on this flight.\n");
         return;
     }
 
-    for (int i = 0; i < num_booked_flights; ++i) {
-        printf("%s, %s, %s, %s, %s, %s, %d, %.2f,%d,%d\n",
-               booked_flights[i].flight_no,
-               booked_flights[i].airlines,
-               booked_flights[i].origin,
-               booked_flights[i].destination,
-               booked_flights[i].departure_time,
-               booked_flights[i].arrival_time,
-               booked_flights[i].flight_hours,
-               booked_flights[i].price,
-               booked_flights[i].seats_left,
-               booked_flights[i].seats_booked);
-    }
+    selectedFlight->seatsLeft--;
+    selectedFlight->seatsBooked++;
+
+    bookedFlights[bookedCount++] = *selectedFlight;
+
+    saveFlights("allFlights.txt", allFlights, allCount);
+    saveFlights("bookedFlights.txt", bookedFlights, bookedCount);
+
+    printf("Flight booked successfully!\n");
 }
 
-// Function to print a single flight ticket
-void print_ticket() {
-    char flight_no[10];
-    printf("Enter Flight No of the flight you want to print the ticket for: ");
-    scanf("%s", flight_no);
-    for (int i = 0; i < num_booked_flights; ++i) {
-        if (strcmp(booked_flights[i].flight_no, flight_no) == 0) {
-            printf("Flight No: %s\nAirlines: %s\nOrigin: %s\nDestination: %s\nDeparture Time: %s\nArrival Time: %s\nFlight Hours: %d\nPrice: %.2f\nSeats Left: %d\nSeats Booked: %d\n",
-                   booked_flights[i].flight_no,
-                   booked_flights[i].airlines,
-                   booked_flights[i].origin,
-                   booked_flights[i].destination,
-                   booked_flights[i].departure_time,
-                   booked_flights[i].arrival_time,
-                   booked_flights[i].flight_hours,
-                   booked_flights[i].price,
-                   booked_flights[i].seats_left,
-                   booked_flights[i].seats_booked);
-            return;
-        }
+void deleteBookedFlight() {
+    Flight bookedFlights[MAX_FLIGHTS];
+    int bookedCount;
+    loadFlights("bookedFlights.txt", bookedFlights, &bookedCount);
+
+    if (bookedCount == 0) {
+        printf("No booked flights to delete.\n");
+        return;
     }
-    printf("No flight found with this Flight No.\n");
+
+    printf("Booked Flights:\n");
+    for (int i = 0; i < bookedCount; i++) {
+        printf("%d. %s (%s) - %s to %s\n",
+               i + 1, bookedFlights[i].flightNo, bookedFlights[i].airline,
+               bookedFlights[i].origin, bookedFlights[i].destination);
+    }
+
+    int choice;
+    printf("Enter the number of the flight you want to delete: ");
+    scanf("%d", &choice);
+
+    if (choice < 1 || choice > bookedCount) {
+        printf("Invalid choice.\n");
+        return;
+    }
+
+    for (int i = choice - 1; i < bookedCount - 1; i++) {
+        bookedFlights[i] = bookedFlights[i + 1];
+    }
+
+    bookedCount--;
+    saveFlights("bookedFlights.txt", bookedFlights, bookedCount);
+
+    printf("Flight deleted successfully!\n");
+}
+
+void printFlightTicket() {
+    Flight bookedFlights[MAX_FLIGHTS];
+    int bookedCount;
+    loadFlights("bookedFlights.txt", bookedFlights, &bookedCount);
+
+    if (bookedCount == 0) {
+        printf("No booked flights to print.\n");
+        return;
+    }
+
+    printf("Booked Flights:\n");
+    for (int i = 0; i < bookedCount; i++) {
+        printf("%d. %s (%s) - %s to %s\n",
+               i + 1, bookedFlights[i].flightNo, bookedFlights[i].airline,
+               bookedFlights[i].origin, bookedFlights[i].destination);
+    }
+
+    int choice;
+    printf("Enter the number of the flight ticket to print: ");
+    scanf("%d", &choice);
+
+    if (choice < 1 || choice > bookedCount) {
+        printf("Invalid choice.\n");
+        return;
+    }
+
+    Flight *ticket = &bookedFlights[choice - 1];
+    printf("\n--- Flight Ticket ---\n");
+    printf("Flight No: %s\nAirline: %s\nOrigin: %s\nDestination: %s\nDeparture: %s\nArrival: %s\nFlight Hours: %.2f\nPrice: $%.2f\n",
+           ticket->flightNo, ticket->airline, ticket->origin, ticket->destination,
+           ticket->departureTime, ticket->arrivalTime, ticket->flightHours,
+           ticket->price);
+    printf("---------------------\n");
 }
 
 int main() {
-    load_flights("allFlights.txt");
+    int choice;
 
-    while (1) {
+    do {
         printf("\nFlight Booking System\n");
-        printf("1. Book a new flight\n");
-        printf("2. Delete a booked flight\n");
-        printf("3. View booked flights\n");
-        printf("4. Print flight ticket\n");
+        printf("1. Book a New Flight\n");
+        printf("2. Delete a Booked Flight\n");
+        printf("3. View Booked Flights\n");
+        printf("4. Print Flight Ticket\n");
         printf("5. Quit\n");
-        int choice;
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                book_flight();
-                save_flights("flights.txt");
+                bookNewFlight();
                 break;
             case 2:
-                delete_flight();
+                deleteBookedFlight();
                 break;
             case 3:
-                view_flights();
+                viewBookedFlights();
                 break;
             case 4:
-                print_ticket();
+                printFlightTicket();
                 break;
             case 5:
-                save_flights("flights.txt");
-                exit(0);
+                printf("Exiting the program.\n");
+                break;
             default:
-                printf("Invalid choice, please try again.\n");
+                printf("Invalid choice. Please try again.\n");
         }
-    }
+    } while (choice != 5);
 
     return 0;
 }
